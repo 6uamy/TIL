276e5f83ab7e4ad9b9ef2c5b5239c1bdca92dc86
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-06-27 19:46:41 +0900

    Update 스택.cs

--- Algorithm/자료구조/스택.cs
+++ Algorithm/자료구조/스택.cs
@@ -2,45 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace stack1{
-  class Program{
-  static void Main(string[] args)
-  {
-      int count = int.Parse(Console.ReadLine());
-      List<string> stack = new List<string>();
-      
-      string[] input;
-      StringBuilder a = new StringBuilder();
-      
-      for(int i=0; i<count; i++){
-        a.Clear();
-        input = Console.ReadLine().Split();
+namespace stack1
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+          int count = int.Parse(Console.ReadLine());
+          List<string> stack = new List<string>();
 
-        switch (input[0])
-        {
-            case "push":
-            stack.Add(input[1]);
-            break;
-            case "pop":
-            a.Append(stack.Count == 0 ? "-1" : stack[stack.Count -1]);  
-            Console.WriteLine(a);
-            if(stack.Count != 0)
-                stack.RemoveAt(stack.Count -1);
-            break;
-            case "size":
-            a.Append(stack.Count);
-            Console.WriteLine(a);
-            break;
-            case "empty":
-            a.Append(stack.Count == 0 ? "1" : "0");
-            Console.WriteLine(a);
-            break;
-            case "top":
-            a.Append(stack.Count == 0 ? "-1" : stack[stack.Count -1]);  
-            Console.WriteLine(a);
-            break;
+          string[] input;
+          StringBuilder a = new StringBuilder();
+
+            for(int i=0; i<count; i++){
+                a.Clear();
+                input = Console.ReadLine().Split();
+
+                switch (input[0])
+                {
+                    case "push":
+                    stack.Add(input[1]);
+                    break;
+                    case "pop":
+                    a.Append(stack.Count == 0 ? "-1" : stack[stack.Count -1]);  
+                    Console.WriteLine(a);
+                    if(stack.Count != 0)
+                        stack.RemoveAt(stack.Count -1);
+                    break;
+                    case "size":
+                    a.Append(stack.Count);
+                    Console.WriteLine(a);
+                    break;
+                    case "empty":
+                    a.Append(stack.Count == 0 ? "1" : "0");
+                    Console.WriteLine(a);
+                    break;
+                    case "top":
+                    a.Append(stack.Count == 0 ? "-1" : stack[stack.Count -1]);  
+                    Console.WriteLine(a);
+                    break;
+                  }
+              }
           }
-        }
       }
-    }
   }
